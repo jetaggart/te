b10b317fb6bdbac7accd492259c54394ea72d7f1
Jeff Taggart <jeff@jetaggart.com>
2015-02-21 16:28:00 -0700

    Beginning of init

--- src/Main.hs
+++ src/Main.hs
@@ -7,10 +7,22 @@ import Shelly
 import Data.List
 import System.Environment
 import System.Process
+import System.Exit
 import Data.Text (pack)
+
+
+executeTests :: [String] -> IO ()
+executeTests testArgs = rawSystem "rspec" testArgs >> return ()
 
 main :: IO ()
 main = shelly $ verbosely $ do
   args <- liftIO getArgs
-  liftIO $ rawSystem "rspec" args
+  let teCommand = head args
+      testArgs = tail args
+  
+  case teCommand of
+    "run" -> liftIO $ executeTests testArgs
+    "init" -> echo "nothing yet"
+    otherwise -> echo "bad te argument"
+
   return ()
