ebab9a8a8bf6d3156a226a42a99ff05a16383524
Jeff Taggart <jeff@jetaggart.com>
2015-02-21 17:18:46 -0700

    Listen works

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
 module Main where
 
@@ -8,21 +10,37 @@ import Data.List
 import System.Environment
 import System.Process
 import System.Exit
+import Control.Monad
 import Data.Text (pack)
 
+teRun :: [String] -> Sh ()
+teRun testArgs = do 
+  let stringArgs = intercalate " " testArgs
+      testCommand ="\"rspec " ++ stringArgs ++ "\""
+  liftIO $ rawSystem ("echo " ++ testCommand ++ " > .te-pipe") []
+  return ()
+ 
 
-executeTests :: [String] -> Sh ()
-executeTests testArgs = liftIO $ rawSystem "rspec" testArgs >> return ()
+teInit :: Sh ()
+teInit = do
+  cmd "mkfifo" ".te-pipe"
+
+teListen :: Sh ()
+teListen = escaping False $  forever $ do
+  run_ "cat .te-pipe | sh" []
+  return ()
 
 main :: IO ()
 main = shelly $ verbosely $ do
   args <- liftIO getArgs
+
   let teCommand = head args
       testArgs = tail args
   
   case teCommand of
-    "run" -> executeTests testArgs
-    "init" -> echo "nothing yet"
+    "run" -> teRun testArgs
+    "init" -> teInit
+    "listen" -> teListen
     otherwise -> echo "bad te argument"
 
   return ()
